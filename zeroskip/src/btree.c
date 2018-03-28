scantist-ossops-m2/cyrus-imapd

commit 761d2d03d034df252a31ab026ea92be62b5308ff

btree: Use the shorter length for comparison
I think this is the 'right thing'[tm] to do.

zeroskip/src/btree.c
@@ -519,12 +519,8 @@ unsigned int btree_memcmp(unsigned char *key, size_t keylen,
 
                 b = (unsigned char*)recs[pos]->key;
 
-                if (keylen < recs[pos]->keylen)
-                        goto lessthan;
-                if (keylen > recs[pos]->keylen)
-                        goto greaterthan;
-
-                c = memcmp(k, b, keylen);
+                c = memcmp(k, b, (keylen < recs[pos]->keylen ?
+                                  keylen : recs[pos]->keylen));
                 if (c == 0)
                         goto equals;
                 if (c < 0)

commit 0d816c1393abb922f9fc7c05e948212d14bc356c

btree: Fix the btree_memcmp function.
This reverts commit 761d2d03d034df252a31ab026ea92be62b5308ff, which
broke the comparator.

zeroskip/src/btree.c
@@ -519,8 +519,12 @@ unsigned int btree_memcmp(unsigned char *key, size_t keylen,
 
                 b = (unsigned char*)recs[pos]->key;
 
-                c = memcmp(k, b, (keylen < recs[pos]->keylen ?
-                                  keylen : recs[pos]->keylen));
+                if (keylen < recs[pos]->keylen)
+                        goto lessthan;
+                if (keylen > recs[pos]->keylen)
+                        goto greaterthan;
+
+                c = memcmp(k, b, keylen);
                 if (c == 0)
                         goto equals;
                 if (c < 0)